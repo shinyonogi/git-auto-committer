cb1f0bc1a7531cae495be6b95467562d0874b7f8
Shintaro Onogi <shinyonogi@outlook.de>
2023-06-28 11:47:47 +0200

    yay

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 #DECLARING: a file name starting with 0x
 file_name = '0x'
+#This is how many times I commit
+commit_times = random.randint(5, 15)
 
 #FUNCTION: Takes a natural number n as an argument and returns the HEX
 def convert_into_16(n):
@@ -31,21 +33,20 @@ def convert_into_16(n):
 
     return num_converted
 
-#This is how many times I commit
-commit_times = random.randint(5, 15)
-
+#LOOP: As many times as I should commit on a day
 for c in range(commit_times):
     #CREATING: Randomly generates a 5bit file name
     for i in range(10):
         file_name += str(convert_into_16(random.randint(0, 15)))
 
+    #ATTENTION: Path name is going to be different after you change the directory below
     match c:
         case 0:
-            #CREATING: Touch a file with the created file name
             path_name = 'desktop/git-random-push/' + file_name
         case _:
             path_name = file_name
 
+    #CREATING: Touch a file with the created file name
     Path(path_name).touch()
 
     #Writes the sentence into the file
@@ -54,6 +55,7 @@ for c in range(commit_times):
     f.close()
 
     #Changing directory (CD)
+    #ATTENTION: Since you commit multiply times -> Changing the directory multiple times does not work
     try:
         cd = 'desktop/git-random-push/'
         os.chdir(cd)
@@ -61,16 +63,17 @@ for c in range(commit_times):
     except FileNotFoundError:
         pass
 
-    #Executes git add . git commit -m "cat" git push
+    #DEFINE: the git commands as Strings
     git_add = 'git add .'
     git_commit = 'git commit -m "cat"'
     git_push = 'git push'
 
+    #EXECUTES: git add . git commit -m "cat" git push
     os.system(git_add)
     os.system(git_commit)
     os.system(git_push)
 
-    #Reset the file name and path name so that it can be looped
+    #RESET: the file name and path name so that it can be looped
     file_name = '0x'
     path_name = ''
 
