25b48e7dc02181853db281d1ed8ab4b0f0c5324e
Shintaro Onogi <shinyonogi@outlook.de>
2023-09-15 19:30:35 +0200

    Added error handling to git actions

--- git_things.py
+++ git_things.py
@@ -1,6 +1,14 @@
 import os
+import logging
 
 def git_commit_and_push(commit_message):
-    os.system("git add .")
-    os.system(f'git commit -m "{commit_message}"')
-    os.system("git push")
+    if os.system("git add .") != 0:
+        logging.error("Failed to execute 'git add .'.")
+        return
+
+    if os.system(f'git commit -m "{commit_message}"') != 0:
+        logging.error(f"Failed to execute 'git commit -m \"{commit_message}\"'.")
+        return
+
+    if os.system("git push") != 0:
+        logging.error("Failed to execute 'git push'.")
