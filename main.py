4ae39436885865a0fe150b8117f46b87d8e47f2e
Shintaro Onogi <shinyonogi@outlook.de>
2023-09-15 18:56:10 +0200

    Moved getenv outside the loop

--- main.py
+++ main.py
@@ -21,16 +21,15 @@ def main():
     use_telegram = get_env_use_api('USE_TELEGRAM').lower() == 'true'
     use_line = get_env_use_api('USE_LINE').lower() == 'true'
 
+    if use_openai:
+        openai.api_key = get_env_variable('OPENAI_API_KEY')
+
     random_address_name = "0x" + "".join([convert_to_hex(random.randint(0, 15)) for _ in range(10)])
     file_name = f"randomAddresses/{random_address_name}"
     Path(file_name).touch()
 
     for _ in range(random.randint(5, 15)):
-        if use_openai:
-            openai.api_key = get_env_variable('OPENAI_API_KEY')
-            commit_message = gpt_commit_message_generation()
-        else:
-            commit_message = "Default Commit Message"
+        commit_message = gpt_commit_message_generation() if use_openai else "Default Commit Message"
 
         if use_line:
             send_line_message(commit_message)
