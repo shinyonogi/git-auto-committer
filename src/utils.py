95da7f96925d1b53aa87da62ba77a91d7dc5627c
shinyonogi <shinyonogi@outlook.de>
2023-09-16 12:45:23 +0200

    Default Commit Message

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ def get_env_use_api(key, default=None):
         return False
     if value.lower() not in ["true", "false"]:
         raise ValueError(f"Invalid value for {key}. Expected 'true' or 'false'.")
-    return value.lower() == "true"
+    return value == "true"
 
 def get_env_variable(key, default=None):
     value = os.environ.get(key)
